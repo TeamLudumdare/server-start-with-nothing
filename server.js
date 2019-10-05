6a926b965962f92a88832c9b0cec245044cde7fc
Anderson Santos De Medeiros <anderson.medeiros97@imd.local>
2019-10-05 20:10:41 -0300

    Criado função que cria um lobby

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const server = require('http').createServer()
 const mongoose = require('mongoose')
-const { player, lobby } = require('./models')
+const crypto = require('crypto')
+const { Player, Lobby } = require('./models')
 
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true})
 
-const io = require('socket.io')(server, {
+const socket = require('socket.io')(server, {
     path: '/socket.io'
+})
+
+socket.on('CreateRoom', (name) => {
+    let player = new Player(
+        {
+            name: name,
+            lifePoints: 100,
+            userID: 1
+        }
+    ).save((err) => {
+        if (err) socket.emit('ErrorLobby', 'Sorry, it wasn´t possible to create a lobby')
+        else {
+            socket.emit('InfoUser', player)
+            let lobbyID = crypto.createHash('sha1').update(Date.now()).digest('hex')
+            let lobby = new Lobby(
+                {
+                    players: 1,
+                    id: lobbyID,
+                    playersData: [player],
+                    host: player
+                }
+            ).save((err) => {
+                if (err) socket.emit('ErrorLobby', 'Sorry, it wasn´t possible to create a lobby')
+                else {
+                    socket.emit('InfoLobby', lobby)
+                }
+            })
+        }
+    })
 })
 
 server.listen(3000)
