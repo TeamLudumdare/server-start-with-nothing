fb16093057ef2255105e175243675ba5afe8c66a
Anderson Santos De Medeiros <anderson.medeiros97@imd.local>
2019-10-06 19:25:51 -0300

    Corrigindo problemas

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ io.on('connection', (socket) => {
         Lobby.where({ room: data.room }).findOne((err, lobby) => {
             if (err) socket.emit('ErrorLobby', { 'error': 'Sorry, it wasn´t possible to enter on lobby' })
             else {
-                if (lobby.players >= 4) {
+                if (lobby.playersData.length >= 4) {
                     socket.emit('ErrorLobby',  { 'error': 'Sorry, but the lobby is full of players' })
                 } else {
                     Player.create(
@@ -71,7 +71,7 @@ io.on('connection', (socket) => {
     socket.on('StartGame', (data) => {
         Lobby.where({ _id: data._id }).findOne((err, lobby) => {
             if (err) socket.emit('ErrorLobby', { 'error': 'Sorry, it wasn´t possible to start the game' })
-            else if (lobby.playersData < 4) {
+            else if (lobby.playersData.length < 4) {
                 socket.emit('ErrorLobby', { 'error': 'Sorry, theresn´t enough players to start the game' })
             } else {
                 Match.create(
