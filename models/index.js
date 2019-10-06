7bdf94ed03e72b40e83db6b7ae2fa32a71db75c6
Anderson Santos De Medeiros <anderson.medeiros97@imd.local>
2019-10-06 14:25:49 -0300

    Criando novo modelo de log da rodada

--- models/index.js
+++ models/index.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
+const Item = mongoose.model('Item', new mongoose.Schema({
+    sprite: String,
+    itemId: Number
+}))
+
 const Player = mongoose.model('Player', new mongoose.Schema({
     lifePoints: { type: Number, default: 100 },
     name: String,
-    alive: { type: Boolean, default: true }
+    alive: { type: Boolean, default: true },
+    items: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item'
+    }]
 }))
 
 const Lobby = mongoose.model('Lobby', new mongoose.Schema({
@@ -28,6 +37,10 @@ const Action = mongoose.model('Action', new mongoose.Schema({
         ref: 'Player'
     },
     targetAll: Boolean,
+    item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item'
+    },
     actionType: String
 }))
 
@@ -53,7 +66,23 @@ const Match = mongoose.model('Match', new mongoose.Schema({
     }]
 }))
 
+const RoundLog = mongoose.model('RoundLog', new mongoose.Schema({
+    playersRead: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Player'
+    }],
+    actions: [{
+        missed: Boolean,
+        action: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Action'
+        }
+    }]
+}))
+
+module.exports.Item = Item
 module.exports.Player = Player
 module.exports.Lobby = Lobby
 module.exports.Action = Action
 module.exports.Match = Match
+module.exports.RoundLog = RoundLog
