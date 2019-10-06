cdbdaf6919ecfee37d5a86e3c85071d7fd9ccdcc
Anderson Santos De Medeiros <anderson.medeiros97@imd.local>
2019-10-05 21:57:33 -0300

    Corrigindo socket, agora ele escuta os eventos

--- server.js
+++ server.js
@@ -1,65 +1,68 @@
 const server = require('http').createServer()
 const mongoose = require('mongoose')
-const crypto = require('crypto')
 const { Player, Lobby } = require('./models')
 
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true})
 
-const socket = require('socket.io')(server, {
+const io = require('socket.io')(server, {
     path: '/socket.io'
 })
 
-socket.on('CreateRoom', (data) => {
-    let player = new Player(
-        {
-            name: data.nome,
-            lifePoints: 100
-        }
-    ).save((err) => {
-        if (err) socket.emit('ErrorLobby',  { 'error': 'Sorry, it wasn´t possible to create a lobby' })
-        else {
-            socket.emit('InfoUser', player)
-            let lobby = new Lobby(
-                {
-                    players: 1,
-                    playersData: [player],
-                    host: player
-                }
-            ).save((err) => {
-                if (err) socket.emit('ErrorLobby',  { 'error': 'Sorry, it wasn´t possible to create a lobby' })
-                else {
-                    socket.emit('InfoLobby', lobby)
-                }
-            })
-        }
-    })
-})
+io.on('connection', (socket) => {
 
-socket.on('JoinRoom', (data) => {
-    query = Lobby.where({ _id: data.id }).findOne((err, lobby) => {
-        if (err) socket.emit('ErrorLobby', { 'error': 'Sorry, it wasn´t possible to enter on lobby' })
-        else {
-            if (lobby.playersData.length >= 4) {
-                socket.emit('ErrorLobby',  { 'error': 'Sorry, but the lobby is full of players' })
-            } else {
-                let player = new Player(
+    socket.on('CreateRoom', (data) => {
+        console.log(data)
+        let player = new Player(
+            {
+                name: data.nome,
+                lifePoints: 100
+            }
+        ).save((err) => {
+            if (err) socket.emit('ErrorLobby',  { 'error': 'Sorry, it wasn´t possible to create a lobby' })
+            else {
+                socket.emit('InfoUser', player)
+                let lobby = new Lobby(
                     {
-                        name: data.nome,
-                        lifePoints: 100
+                        players: 1,
+                        playersData: [player],
+                        host: player
                     }
                 ).save((err) => {
                     if (err) socket.emit('ErrorLobby',  { 'error': 'Sorry, it wasn´t possible to create a lobby' })
                     else {
-                        lobby.playersData.push(player).save((err) => {
-                            if (err) socket.emit('ErrorLobby',  { 'error': 'Sorry, it wasn´t possible to create a lobby' })
-                            else {
-                                socket.emit('SuccessEnteringLobby', {})
-                            }
-                        })
+                        socket.emit('InfoLobby', lobby)
                     }
                 })
             }
-        }
+        })
+    })
+    
+    socket.on('JoinRoom', (data) => {
+        query = Lobby.where({ _id: data.id }).findOne((err, lobby) => {
+            if (err) socket.emit('ErrorLobby', { 'error': 'Sorry, it wasn´t possible to enter on lobby' })
+            else {
+                if (lobby.playersData.length >= 4) {
+                    socket.emit('ErrorLobby',  { 'error': 'Sorry, but the lobby is full of players' })
+                } else {
+                    let player = new Player(
+                        {
+                            name: data.nome,
+                            lifePoints: 100
+                        }
+                    ).save((err) => {
+                        if (err) socket.emit('ErrorLobby',  { 'error': 'Sorry, it wasn´t possible to create a lobby' })
+                        else {
+                            lobby.playersData.push(player).save((err) => {
+                                if (err) socket.emit('ErrorLobby',  { 'error': 'Sorry, it wasn´t possible to create a lobby' })
+                                else {
+                                    socket.emit('SuccessEnteringLobby', {})
+                                }
+                            })
+                        }
+                    })
+                }
+            }
+        })
     })
 })
 
